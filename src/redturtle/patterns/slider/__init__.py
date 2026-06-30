a7dd7f3814d6f91bfe054ec7629d04e9f3c027dd
fabio.iaquinta <fabi.iaquinta@gmail.com>
2026-06-30 14:17:24 +0200

    [FIX] removed all references to collective.editablemenu

--- src/redturtle/patterns/slider/__init__.py
+++ src/redturtle/patterns/slider/__init__.py
@@ -2,5 +2,9 @@
 """Init and utils."""
 
 from zope.i18nmessageid import MessageFactory
+from logging import getLogger
 
-_ = MessageFactory('redturtle.patterns.slider')
+PACKAGE_NAME='redturtle.patterns.slider'
+
+logger = getLogger(PACKAGE_NAME)
+_ = MessageFactory(PACKAGE_NAME)
